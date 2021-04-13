437f8842fd91cb5ed69f4bb0169a3ce7e3cc4bb2
Adryzz <46694241+adryzz@users.noreply.github.com>
2021-04-13 09:02:15 +0200

    add serial connection method

---
+++ RemoteControlV2/Connection/IConnectionMethod.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RemoteControlV2.Connection
+{
+    public interface IConnectionMethod : IDisposable
+    {
+        void Initialize();
+
+        event EventHandler OnCommandReceived;
+
+        void SendText(string text);
+    }
+}
---
+++ RemoteControlV2/Connection/SerialConnectionMethod.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO.Ports;
+
+namespace RemoteControlV2.Connection
+{
+    public class SerialConnectionMethod : IConnectionMethod
+    {
+        public event EventHandler OnCommandReceived;
+
+        public SerialPort Port { get; private set; }
+
+        public SerialConnectionMethod(string port, int baud)
+        {
+            Port = new SerialPort(port, baud, Parity.None, 8, StopBits.One);
+        }
+
+        public void Initialize() => Port.Open();
+
+        public void SendText(string text) => Port.Write(text);
+
+        public void Dispose() => Port.Dispose();
+    }
+}
