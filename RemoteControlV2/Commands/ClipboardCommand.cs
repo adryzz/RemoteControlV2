16b0362f8649ce0b104a4328656f60ab2fba5718
Adryzz <46694241+adryzz@users.noreply.github.com>
2021-04-15 01:20:11 +0200

    add clipboard command and version

---
+++ RemoteControlV2/Commands/ClipboardCommand.cs
@@ -0,0 +1,44 @@
+﻿using RemoteControlV2.Plugins;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RemoteControlV2.Commands
+{
+    class ClipboardCommand : ICommand
+    {
+        public string Name => "clipboard";
+
+        public string Syntax => "Usage: 'clipboard get' or 'clipboard set <text>' or 'clipboard clear'";
+
+        public bool Enabled { get; set; } = true;
+
+        public void Execute(string arguments)
+        {
+            string[] arr = arguments.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+            switch(arr[0])
+            {
+                case "get":
+                    {
+                        Program.Connection.SendLine(Clipboard.GetText());
+                        return;
+                    }
+                case "set":
+                    {
+                        Program.Connection.SendLine("Done!");
+                        Clipboard.SetText(arguments.Remove(0, 4));
+                        return;
+                    }
+                case "clear":
+                    {
+                        Clipboard.Clear();
+                        Program.Connection.SendLine("Done!");
+                        return;
+                    }
+            }
+        }
+    }
+}
---
+++ RemoteControlV2/Commands/VersionCommand.cs
@@ -0,0 +1,24 @@
+﻿using RemoteControlV2.Plugins;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RemoteControlV2.Commands
+{
+    class VersionCommand : ICommand
+    {
+        public string Name => "version";
+
+        public string Syntax => "Usage: 'version'";
+
+        public bool Enabled { get; set; }
+
+        public void Execute(string arguments)
+        {
+            Program.Connection.SendLine(Application.ProductVersion);
+        }
+    }
+}
