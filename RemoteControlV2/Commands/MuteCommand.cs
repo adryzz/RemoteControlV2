5b8bb383e39ddd16b1f8c2ce38091cb34bacb3e3
Adryzz <46694241+adryzz@users.noreply.github.com>
2021-04-16 14:09:11 +0200

    add device selection to mute command

--- RemoteControlV2/Commands/MuteCommand.cs
+++ RemoteControlV2/Commands/MuteCommand.cs
@@ -12,7 +12,7 @@ namespace RemoteControlV2.Commands
     {
         public string Name => "mute";
 
-        public string Syntax => "Usage: 'mute <state>'";
+        public string Syntax => "Usage: 'mute <state>' or 'mute <device> state'";
 
         public bool Enabled { get; set; } = true;
 
@@ -20,13 +20,55 @@ namespace RemoteControlV2.Commands
 
         public void Execute(string arguments)
         {
-            var state = CommandParser.BooleanParser(arguments);
-            if (!state.HasValue)
+            string[] arr = arguments.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+            if (arr.Length < 2)
             {
-                throw new ArgumentException();
+                var state = CommandParser.BooleanParser(arguments);
+                if (!state.HasValue)
+                {
+                    throw new ArgumentException();
+                }
+                controller.DefaultPlaybackDevice.Mute(state.Value);
+                Program.Connection.SendLine("Mute set to " + state.ToString());
             }
-            controller.DefaultPlaybackDevice.Mute(state.Value);
-            Program.Connection.SendLine("Mute set to " + state.ToString());
+            else
+            {
+                var number = CommandParser.Int32Parser(arr[0]);
+                if (number.HasValue)
+                {
+                    var state = CommandParser.BooleanParser(arr[1]);
+                    if (!state.HasValue)
+                    {
+                        throw new ArgumentException();
+                    }
+                    List<CoreAudioDevice> dev = controller.GetPlaybackDevices().ToList();
+                    dev[number.Value].Mute(state.Value);
+                    Program.Connection.SendLine($"Mute of device {number} set to {state}");
+                }
+                else
+                {
+                    string name = "";
+                    for (int i = 0; i < arr.Length - 1; i++)
+                    {
+                        name += arr[i] + " ";
+                    }
+                    name = name.Remove(name.Length - 1).ToLower();
+                    var state = CommandParser.BooleanParser(arr[1]);
+                    if (!state.HasValue)
+                    {
+                        throw new ArgumentException();
+                    }
+                    List<CoreAudioDevice> dev = controller.GetPlaybackDevices().ToList();
+                    foreach (CoreAudioDevice d in dev)
+                    {
+                        if (d.Name.ToLower().Contains(name) || d.Name.ToLower().Contains(name) || d.InterfaceName.ToLower().Contains(name))
+                        {
+                            d.Mute(state.Value);
+                            Program.Connection.SendLine($"Mute of device {d.FullName} set to {state}");
+                        }
+                    }
+                }
+            }
         }
     }
 }
