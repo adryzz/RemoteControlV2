cfefddb9cbc770cfc0167e7768b62c4206d19dcb
Adryzz <46694241+adryzz@users.noreply.github.com>
2021-04-16 13:03:28 +0200

    add device choosing for volume command

--- RemoteControlV2/Commands/VolumeCommand.cs
+++ RemoteControlV2/Commands/VolumeCommand.cs
@@ -12,7 +12,7 @@ namespace RemoteControlV2.Commands
     {
         public string Name => "volume";
 
-        public string Syntax => "Usage: 'volume <percentage>'";
+        public string Syntax => "Usage: 'volume <percentage>' or 'volume <device> <percentage>'";
 
         public bool Enabled { get; set; } = true;
 
@@ -20,13 +20,56 @@ namespace RemoteControlV2.Commands
 
         public void Execute(string arguments)
         {
-            var value = CommandParser.Int32Parser(arguments);
-            if (!value.HasValue)
+            arguments = arguments.Replace("%", "");
+            string[] arr = arguments.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+            if (arr.Length < 2)
             {
-                throw new ArgumentException();
+                var value = CommandParser.Int32Parser(arr[0]);
+                if (!value.HasValue)
+                {
+                    throw new ArgumentException();
+                }
+                controller.DefaultPlaybackDevice.Volume = value.Value;
+                Program.Connection.SendLine($"Volume set to {value}%");
             }
-            controller.DefaultPlaybackDevice.Volume = value.Value;
-            Program.Connection.SendLine("Volume set to " + value.ToString() + "%");
+            else
+            {
+                var number = CommandParser.Int32Parser(arr[0]);
+                if (number.HasValue)
+                {
+                    var volume = CommandParser.Int32Parser(arr[1]);
+                    if (!volume.HasValue)
+                    {
+                        throw new ArgumentException();
+                    }
+                    List<CoreAudioDevice> dev = controller.GetPlaybackDevices().ToList();
+                    dev[number.Value].Volume = volume.Value;
+                    Program.Connection.SendLine($"Volume of device {number} set to {volume}%");
+                }
+                else
+                {
+                    string name = "";
+                    for(int i = 0; i < arr.Length - 1; i++)
+                    {
+                        name += arr[i] + " ";
+                    }
+                    name = name.Remove(name.Length-1).ToLower();
+                    var volume = CommandParser.Int32Parser(arr[arr.Length-1]);
+                    if (!volume.HasValue)
+                    {
+                        throw new ArgumentException();
+                    }
+                    List<CoreAudioDevice> dev = controller.GetPlaybackDevices().ToList();
+                    foreach (CoreAudioDevice d in dev)
+                    {
+                        if (d.Name.ToLower().Contains(name) || d.Name.ToLower().Contains(name) || d.InterfaceName.ToLower().Contains(name))
+                        {
+                            d.Volume = volume.Value;
+                            Program.Connection.SendLine($"Volume of device {d.FullName} set to {volume}%");
+                        }
+                    }
+                }
+            }
         }
     }
 }
